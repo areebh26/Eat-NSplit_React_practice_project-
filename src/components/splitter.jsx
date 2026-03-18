b4aff9f98581945060a703d302c9fd79222862fe
areeb <areebulhaq2005@gmail.com>
2026-03-18 11:15:52 +0500

    feat: Implement bill splitting UI with state management for bill value, expenses, and payer selection.

--- src/components/splitter.jsx
+++ src/components/splitter.jsx
@@ -1,7 +1,39 @@
-function splitter() {
+import { useState } from "react"
+
+function Splitter(friend) {
+    let [bill,setBill]=useState(0);
+    let [yourExpense,setYourExpense]=useState(0);
+    let [select,setSelect] = useState("You");
   return (
-    <div>splitter</div>
+    <div>
+        <div>
+            <div>
+                <h2>SPLIT A BILL WITH {friend.name}</h2>
+            </div>
+            <div>
+                 <label for="bill">Bill Value : </label>
+                 <input type="number" id="bill" value={bill} onChange={(e)=>(setBill(Number(e.target.value)))} />
+            </div>
+            <div>
+                 <label for="expense">Your expense : </label>
+                 <input type="number" id="expense" value={yourExpense} onChange={(e)=>(setYourExpense(Number(e.target.value)))} />
+            </div>
+            <div>
+                <span>{friend.name}'s Expense : </span><span>{bill-yourExpense}</span>
+            </div>
+            <div>
+                <label for="select">Who is going to pay the bill ? </label>
+                <select  id="select" value={select} onChange={(e)=>(setSelect(e.target.value))}>
+                    <option value="You"></option>
+                    <option value={friend.name}>{friend.name}</option>
+                </select>
+            </div>
+            <div>
+                <button>Split Bill</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
-export default splitter
+export default Splitter
