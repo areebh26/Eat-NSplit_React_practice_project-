5c8fe6cd0d78d1e00b86e28ea0be9ecc7633b9cb
areeb <areebulhaq2005@gmail.com>
2026-03-18 07:08:55 +0500

    feat: Implement controlled input components for friend name and image URL using React state.

--- src/components/addFriendForm.jsx
+++ src/components/addFriendForm.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react"
+
 export default function AddFriendForm(){
+    let [name,setName] = useState("");
+    let [image,setImage] = useState("");
     return (
         <div>
             <div>
                 <div>
                     <label for="name">Friend Name : </label>
-                    <input type="text" id="name" />
+                    <input type="text" id="name" value={name} onChange={(e)=>(setName(e.target.value))} />
                 </div>
                 <div>
                     <label for="imageURL">Image URL : </label>
-                    <input type="text" id="imageURL" />
+                    <input type="text" id="imageURL" value={image} onChange={(e)=>(setImage(e.target.value))}/>
                 </div>
                 <button>Add</button>
             </div>
